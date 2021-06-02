c5cb1ef4b5bd688a86067d4cc3a618d3c1ee0661
Alec Gunny <alec.gunny@gmail.com>
2021-06-01 19:49:02 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import logging
+import sys
+import time
+
+import typeo
+from stillwater import ThreadedMultiStreamClient
+from stillwater.utils import ExceptionWrapper
+from utils import GwfFrameFileDataSource, GwfFrameFileWriter
+
+
+def main(
+    url: str,
+    model_name: str,
+    model_version: int,
+    input_pattern: str,
+    channels: str,
+    sample_rate: int,  # TODO: can get from model config,
+    output_pattern: str
+):
+    with open(channels, "r") as f:
+        channels = f.read().splitlines()
+
+    client = ThreadedMultiStreamClient(
+        url, model_name, model_version, name="client"
+    )
+    writer = GwfFrameFileWriter(output_pattern, channels[0], name="writer")
+
+    source = GwfFrameFileDataSource(
+        input_pattern, channels, sample_rate, name="reader"
+    )
+    writer.add_parent(source)
+    client.add_source(source, child=writer)
+
+    conn_out = writer.add_child("output")
+    client.start()
+    last_recv_time = time.time()
+    # latency, n = 0.0, 0
+    try:
+        while True:
+            fname = None
+            if conn_out.poll():
+                fname = conn_out.recv()
+            elif (time.time() - last_recv_time) > 3:
+                raise RuntimeError(
+                    "No files written in last 3 seconds, exiting"
+                )
+
+            # make sure our pipeline didn't raise an error
+            if isinstance(fname, ExceptionWrapper):
+                fname.reraise()
+
+            # # update our running latency measurement
+            # for i in range(20):
+            #     try:
+            #         _, t0, __, tf = client._metric_q.get_nowait()
+            #     except queue.Empty:
+            #         break
+            #     n += 1
+            #     latency += (tf - t0 - latency) / n
+
+            # sleep for a second to release the GIL no matter
+            # what happened, then continue if no filenames
+            # got returned
+            time.sleep(0.1)
+            if fname is None:
+                continue
+
+            logging.info(
+                "Wrote cleaned data to {}, average latency {:0.2}f ms".format(
+                    fname, latency * 1000
+                )
+            )
+    except Exception:
+        logging.error("Fatal error:")
+    finally:
+        client.stop()
+        client.join(10)
+        try:
+            client.close()
+        except ValueError:
+            logging.warning("Client closing ungracefully")
+            client.terminate()
+            time.sleep(1)
+            client.close()
+
+
+if __name__ == "__main__":
+    parser = typeo.make_parser(main)
---
+++ utils.py
@@ -0,0 +1,160 @@
+import os
+import pickle
+import queue
+import re
+import typing
+import time
+import multiprocessing as mp
+
+import numpy as np
+from gwpy.timeseries import TimeSeriesDict
+from stillwater import StreamingInferenceProcess, Package
+
+
+def _get_file_gps_timestamp(fname):
+    # subtract one from file creation time to
+    # account for the latency incurred by waiting
+    # for the file to be created
+    file_creation_timestamp = os.stat(fname).st_ctime - 1
+    return file_creation_timestamp
+
+
+class GwfFrameFileDataSource(StreamingInferenceProcess):
+    def __init__(
+        self,
+        input_pattern: str,
+        channels: typing.List[str],
+        kernel_stride: float,
+        sample_rate: float,
+        preproc_file: str,
+        name: typing.Optional[str] = None
+    ):
+        self.input_pattern = input_pattern
+        self.channels = channels
+        self.kernel_stride = kernel_stride
+        self.sample_rate = sample_rate
+        self._update_size = int(kernel_stride * sample_rate)
+
+        self._idx = None
+        self._data = None
+        self._self_q = mp.Queue()
+
+        super().__init__(name=name)
+
+    def __iter__(self):
+        self.start()
+        return self
+
+    def _get_initial_timestamp(self):
+        prefix, postfix = self.input_pattern.split("{}")
+        regex = re.compile(
+            "(?<={})[0-9]{}(?={})".format(prefix, "{10}", postfix)
+        )
+        timestamps = map(regex.search, os.path.dirname(self.input_pattern))
+        if not any(timestamps):
+            raise ValueError(
+                "Couldn't find any timestamps matching the "
+                f"pattern {self.input_pattern}"
+            )
+        timestamps = [int(t.group(0)) for t in timestamps if t is not None]
+        return max(timestamps)
+
+    def _get_data(self):
+        if self._idx is None:
+            self._idx = 0
+            self._t0 = self._get_initial_timestamp()
+
+        start = self._idx * self._update_size
+        stop = (self._idx + 1) * self._update_size
+
+        if self._data is None or stop > self._data.shape[1]:
+            # try to load in the next second's worth of data
+            # if it takes more than a second to get created,
+            # then assume the worst and raise an error
+            start_time = time.time()
+            path = self.path_pattern.format(self._t0)
+            while time.time() - start_time < 3:
+                try:
+                    data = TimeSeriesDict.read(path, self.channels)
+                    break
+                except FileNotFoundError:
+                    time.sleep(1e-3)
+            else:
+                raise ValueError(f"Couldn't find next timestep file {path}")
+            self._latency_t0 = _get_file_gps_timestamp(path)
+
+            # resample the data and turn it into a numpy array
+            data.resample(self.sample_rate)
+            data = np.stack(
+                [data[channel].value for channel in self.channels]
+            ).astype("float32")
+
+            self.children["writer"].send((path, data[0], self._latency_t0))
+            data = data[1:]
+
+            if self._data is not None and start < self._data.shape[1]:
+                leftover = self._data[:, start:]
+                data = np.concatenate([leftover, data], axis=1)
+
+            self._data = data
+            self._t0 += 1
+            self._idx = 0
+
+        # return the next piece of data
+        x = self._data[:, start:stop]
+
+        # offset the frame's initial time by the time
+        # corresponding to the first sample of stream
+        self._idx += 1
+        t0 = self._latency_t0 + self._idx * self.kernel_stride
+        return Package(x=x, t0=t0)
+
+    def _do_stuff_with_data(self, package):
+        self._self_q.put(package)
+
+    def __next__(self):
+        while True:
+            try:
+                return self._self_q.get_nowait()
+            except queue.Empty:
+                time.sleep(1e-6)
+
+
+class GwfFrameFileWriter(StreamingInferenceProcess):
+    def __init__(
+        self,
+        output_dir,
+        channel_name,
+        sample_rate,
+        kernel_stride,
+        name=None
+    ):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        self.output_dir = output_dir
+        self.channel_name = channel_name
+        self.sample_rate = sample_rate
+        self.kernel_stride = kernel_stride
+
+        self._strains = []
+        self._noise = np.array([])
+        super().__init__(name=name)
+
+    def _get_data(self):
+        if self.parents["reader"].poll():
+            stuff = self.parents["reader"].recv()
+            self._strains.append(stuff)
+
+        if self.parents["client"].poll():
+            return self.parents["client"].recv()
+        return None
+
+    def _do_stuff_with_data(self, package):
+        self._noise = self._noise.append(package.x[0, :kernel_stride])
+
+        if len(self._noise) >= self.sample_rate:
+            noise, self._noise = np.split(self._noise, self.sample_rate)
+            fname, strain, t0 = self._strains.pop(0)
+
+            strain = strain
